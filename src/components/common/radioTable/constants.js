70ee60dd8a63f2b33f280d3440ebea126430d3db
annapismenna <pismennaanna@gmail.com>
2018-02-14 19:03:46 +0700

    constants for radio-table 2

--- src/components/common/radioTable/constants.js
+++ src/components/common/radioTable/constants.js
@@ -10,6 +10,16 @@ const radioGroup = {
         { id: 'question-6', name: 'Едите молочные изделия?' },
         { id: 'question-7', name: 'Едите сладoсти?' }
     ],
+
+    radioTable_2: [
+        { id: 'question-1', name: 'Сладости? (конфеты, печеньки)?' },
+        { id: 'question-2', name: 'Соленое? (чипсы, сухарики)' },
+        { id: 'question-3', name: 'Свежие фрукты?' },
+        { id: 'question-4', name: 'Свежие овощи?' },
+        { id: 'question-5', name: 'Мясо?' },
+        { id: 'question-6', name: 'Молочные изделия?' },
+        { id: 'question-7', name: 'Орехи?' }
+    ],
 };
 
 export default radioGroup;
